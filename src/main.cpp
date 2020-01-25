ec05a5b3ef9d79b95c9fb08781b20d2cd3c5c202
eduardomarcos <dudy1984@gmail.com>
2020-01-25 11:06:05 +0100

    aht10 without security and without waiting time

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 
 #define MY_RF24_PA_LEVEL RF24_PA_MIN
 #define MY_RF24_CHANNEL (72)
-#define MY_SECURITY_SIMPLE_PASSWD "dell-nox-simple-password"
 
 #define MY_NODE_ID 1
 
@@ -26,6 +25,7 @@ float lastTemperatureRead = 0;
 float lastHumidityRead = 0;
 
 void readTemperatureAndHumidity();
+float roundMeasurement(float val);
 void processHumidity(float humidity);
 void processTemperature(float temperature);
 
@@ -52,13 +52,9 @@ void loop()
 
 void readTemperatureAndHumidity()
 {
-  wait(2000);
-  // float temperature = aht10.readTemperature();
-  float temperature = 29.9;
-  Serial.println("--->Temp:");
-  Serial.println(temperature);
-  // float humidity = aht10.GetHumidity();
-  float humidity = 0;
+  // wait(2000);
+  float temperature = aht10.readTemperature();
+  float humidity = aht10.readHumidity();
 
   processTemperature(temperature);
   processHumidity(humidity);
@@ -68,12 +64,14 @@ void processTemperature(float temperature)
 {
   if (isnan(temperature))
   {
-    Serial.println("Failed reading temperature from DHT!");
+    Serial.println("Failed reading temperature!");
   }
   else
   {
+    temperature = roundMeasurement(temperature);
     if (temperature != lastTemperatureRead)
     {
+      Serial.println("Sending...");
       send(msgTemp.set(temperature, 1));
       lastTemperatureRead = temperature;
     }
@@ -84,14 +82,27 @@ void processHumidity(float humidity)
 {
   if (isnan(humidity))
   {
-    Serial.println("Failed reading humidity from DHT!");
+    Serial.println("Failed reading humidity!");
   }
   else
   {
+    humidity = roundMeasurement(humidity);
     if (humidity != lastHumidityRead)
     {
       send(msgHum.set(humidity, 1));
       lastHumidityRead = humidity;
     }
   }
+}
+
+float roundMeasurement(float val)
+{
+  Serial.print("\nRounding value: ");
+  Serial.print(val);
+  val = val * 10.0f;
+  val = (val > (floor(val) + 0.5f)) ? ceil(val) : floor(val);
+  val = val / 10.0f;
+  Serial.print(" -> ");
+  Serial.println(val);
+  return val;
 }
