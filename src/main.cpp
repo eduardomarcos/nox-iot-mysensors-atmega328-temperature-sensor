abd88ce8a0c15d6011b7d6d7afd679a58bdc5f82
eduardomarcos <dudy1984@gmail.com>
2020-01-30 19:50:20 +0100

    reading battery directly

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,11 @@
 
 #define MY_NODE_ID 1
 
+#define MIN_V 2800
+#define MAX_V 4130
+
 #include <MySensors.h>
 #include "AHT10.h"
-#include <Battery.h>
 
 static const uint64_t UPDATE_INTERVAL = 5000;
 
@@ -17,7 +19,6 @@ static const uint64_t UPDATE_INTERVAL = 5000;
 
 MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
 MyMessage msgHum(CHILD_ID_HUM, V_HUM);
-Battery battery(3000, 4200, A0);
 
 AHT10 aht10;
 
@@ -25,6 +26,7 @@ float lastTemperatureRead = 0;
 float lastHumidityRead = 0;
 int oldBatteryPcnt = 0;
 
+long readVcc();
 void readBatteryLevel();
 void readTemperatureAndHumidity();
 float roundMeasurement(float val);
@@ -40,7 +42,7 @@ void presentation()
 
 void setup()
 {
-  battery.begin(5000, 1.0);
+  analogReference(INTERNAL);
   if (aht10.begin())
   {
     Serial.println("AHT10 Connected!");
@@ -105,12 +107,39 @@ float roundMeasurement(float val)
   return val;
 }
 
+long readVcc()
+{
+// Read 1.1V reference against AVcc
+// set the reference to Vcc and the measurement to the internal 1.1V reference
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#elif defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+  ADMUX = _BV(MUX5) | _BV(MUX0);
+#elif defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+  ADMUX = _BV(MUX3) | _BV(MUX2);
+#else
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#endif
+
+  delay(2);            // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA, ADSC))
+    ; // measuring
+
+  uint8_t low = ADCL;  // must read ADCL first - it then locks ADCH
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high << 8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result;              // Vcc in millivolts
+}
+
 void readBatteryLevel()
 {
-  int batteryLevel = battery.level();
-  Serial.print("Battery voltage is ");
-  Serial.print(battery.voltage());
-  Serial.print(" (");
+  long currentVcc = readVcc();
+  int batteryLevel = min(map(currentVcc, MIN_V, MAX_V, 0, 100), 100);
+  Serial.print("Battery level (");
   Serial.print(batteryLevel);
   Serial.println("%)");
   if (oldBatteryPcnt != batteryLevel)
